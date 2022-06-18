58d3242dffeb474730b75561e7707d361c0ccfc0
Gavin Farmer <mgavinfarmer@gmail.com>
2022-06-18 00:07:32 -0400

    Initial commit

---
+++ src/main/java/bm/burger_menu/BurgerMenuApplication.java
@@ -0,0 +1,13 @@
+package bm.burger_menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurgerMenuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bm/burger_menu/BurgerMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package bm.burger_menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerMenuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
